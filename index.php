c52299f4e0a8f1ed6999b47ffc5c74a8b157228a
LelilolZH (Lev) <saminskijl@bzz.ch>
2021-01-23 17:09:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,15 +4,18 @@
 <!DOCTYPE html>
 <html lang="de">
 <head>
+    <title><?php echo MainTitle; ?></title>
+    <link rel="shortcut icon" href="<?php echo generate_Link("/img/logo.svg"); ?>" type="image/svg+xml">
     <?php
         server_include_force($_SERVER["DOCUMENT_ROOT"]."/meta/meta.php");
     ?>
-    <title><?php echo MainTitle; ?></title>
-    <link rel="shortcut icon" href="<?php echo generate_Link("/img/logo.svg"); ?>" type="image/svg+xml">
     <link rel="stylesheet" type="text/css" href="<?php echo generate_Link("/css/main.css"); ?>">
     <link rel="stylesheet" type="text/css" href="<?php echo generate_Link("/css/mobile.css"); ?>" media="only screen and (max-width: 800px)" >
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Overpass:wght@600&display=swap" rel="stylesheet">  
+    <?php
+        //add here new css
+    ?>
 </head>
 <body style="display:none;opacity:0;">
     <div class="app-outher">
